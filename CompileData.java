c8992d790b2f1d54b052a80a0a55eb02dd501f78
pYT0NN <bustmepure@gmail.com>
2018-04-10 17:38:36 +0200

    finished reading in data

--- CompileData.java
+++ CompileData.java
@@ -9,9 +9,15 @@ static int x = 0; //Zähler damit man durch das gesamte Array des inputs geht
 public static Crypto[] makeCrypto() throws IOException {
         Crypto[] out = new Crypto[10];
         download();
+        //getCurrentLine(0); //Nächste Zeile, Zählt hoch
 
         for(int i = 0; i < 10; i++) {
-                getCurrentLine(i); //Nächste Zeile, Zählt hoch
+                getCurrentLine(i);
+
+                long available_supply = 0; //Falls Werte nicht verfügbar
+                long total_supply = 0;
+                long max_supply = 0;
+
 
                 //Werte aus der Zeile auslesen, je nachdem wo releavante Infos anfangen und aufhören
                 String id = line.substring(6, length - 3);
@@ -20,25 +26,39 @@ public static Crypto[] makeCrypto() throws IOException {
                 getCurrentLine(i);
                 String symbol = line.substring(10, length - 3);
                 getCurrentLine(i);
-                // System.out.println(line); //Errortest
-                // System.out.println("x = " + x);
-                // System.out.println("i = " + i);
                 int rank = Integer.parseInt(line.substring(8, length - 3));
                 getCurrentLine(i);
                 double price_usd = Double.parseDouble(line.substring(13, length - 3));
                 getCurrentLine(i);
                 double price_btc = Double.parseDouble(line.substring(13, length - 3));
                 getCurrentLine(i);
-                int volume = Integer.parseInt(line.substring(18, length - 4)); //hier aktueller error
+                long volume = Long.parseLong(line.substring(18, length - 5)); //hier aktueller error
                 getCurrentLine(i);
-                int market_cap = Integer.parseInt(line.substring(18, length - 3));
+                long market_cap = Long.parseLong(line.substring(18, length - 5));
                 getCurrentLine(i);
-                int available_supply = Integer.parseInt(line.substring(20, length - 5));
-                getCurrentLine(i);
-                int total_supply = Integer.parseInt(line.substring(16, length - 5));
-                getCurrentLine(i);
-                int max_supply = Integer.parseInt(line.substring(14, length - 5));
-                getCurrentLine(i);
+
+                try{
+                        available_supply = Long.parseLong(line.substring(20, length - 5));
+                        getCurrentLine(i);
+                }
+                catch(Exception e) {
+                        getCurrentLine(i);
+                }
+                try{
+                        total_supply = Long.parseLong(line.substring(16, length - 5));
+                        getCurrentLine(i);
+                }
+                catch(Exception e) {
+                        getCurrentLine(i);
+                }
+                try{
+                        max_supply = Long.parseLong(line.substring(14, length - 5));
+                        getCurrentLine(i);
+                }
+                catch(Exception e) {
+                        getCurrentLine(i);
+                }
+
                 float percent_change_1h = Float.parseFloat(line.substring(21, length - 3));
                 getCurrentLine(i);
                 float percent_change_24h = Float.parseFloat(line.substring(22, length - 3));
@@ -46,7 +66,7 @@ public static Crypto[] makeCrypto() throws IOException {
                 float percent_change_7d  = Float.parseFloat(line.substring(21, length - 3));
                 getCurrentLine(i);
                 long last_updated  = Long.parseLong(line.substring(16, length - 2));
-                getCurrentLine(i);
+
 
 
                 //Coin Objecte mit den Werten initialisieren
@@ -67,11 +87,11 @@ public static Crypto[] makeCrypto() throws IOException {
 }
 public static void main(String[] args) throws IOException {
         Crypto[] ten = makeCrypto();
-        System.out.println(ten[0].id);
+        System.out.println(ten[4].id);
 }
 
 public static void getCurrentLine(int i){
-        int z = i + x;
+        int z = x;
         x++;
 
         line = in[z]; //Aktuelle Zeile
--- Crypto.java
+++ Crypto.java
@@ -8,17 +8,17 @@ String symbol;
 int rank;
 double price_usd;
 double price_btc;
-int volume;
-int market_cap;
-int available_supply;
-int total_supply;
-int max_supply;
+long volume;
+long market_cap;
+long available_supply;
+long total_supply;
+long max_supply;
 float percent_change_1h;
 float percent_change_24h;
 float percent_change_7d;
 long last_updated;
 
-public Crypto(String id, String name, String symbol, int rank, double price_usd, double price_btc, int volume, int market_cap, int available_supply, int total_supply, int max_supply, float percent_change_1h, float percent_change_24h, float percent_change_7d, long last_updated){
+public Crypto(String id, String name, String symbol, int rank, double price_usd, double price_btc, long volume, long market_cap, long available_supply, long total_supply, long max_supply, float percent_change_1h, float percent_change_24h, float percent_change_7d, long last_updated){
         this.id = id;
         this.name = name;
         this.symbol = symbol;
@@ -34,5 +34,17 @@ public Crypto(String id, String name, String symbol, int rank, double price_usd,
         this.percent_change_7d = percent_change_7d;
         this.last_updated = last_updated;
 }
-
+public Crypto(String id, String name, String symbol, int rank, double price_usd, double price_btc, long volume, long market_cap, float percent_change_1h, float percent_change_24h, float percent_change_7d, long last_updated){
+        this.id = id;
+        this.name = name;
+        this.symbol = symbol;
+        this.rank = rank;
+        this.price_usd = price_usd;
+        this.volume = volume;
+        this.market_cap = market_cap;
+        this.percent_change_1h = percent_change_1h;
+        this.percent_change_24h = percent_change_24h;
+        this.percent_change_7d = percent_change_7d;
+        this.last_updated = last_updated;
+}
 }
